4e913491b67123a50d692ff0b9151d7bd56eaf2b
Slonetech <kengeregordon@gmail.com>
2023-06-21 14:08:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+const gameRecord = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  function superbowlWin(gameRecords) {
+    const winRecord = gameRecords.find(record => record.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
